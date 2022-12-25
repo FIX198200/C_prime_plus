adc60707312c6055ed52909ae0e3b387892deeeb
付迅雨 <121087710+FIX198200@users.noreply.github.com>
2022-12-25 19:29:20 +0800

    Create 啊哈C.cpp

---
+++ 啊哈C.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+//int hcf(int m, int n) {
+//	int temp, i;
+//	if (m < n) {
+//		temp = m;
+//		m = n;
+//		n = temp;
+//	} else {
+//		i = n;
+//		return i;
+//	}
+//	while (i > 0) {
+//		int s;
+//		if (m % i == 0 && n % i == 0) {
+//			return i;
+//			break;
+//		}
+//		i--;
+//	}
+//}
+//int main() {
+//	int m, n,i,s;
+//	printf("输入两个正整数: ");
+//	scanf("%d %d", &m, &n);
+//	printf("%d 和 %d 的最大公约数为 %d", m, n, hcf(m, n));
+//	return 0;
+//}
+
+//黑客帝国数字雨
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//	int a=1;
+//	while (a<=10000) {
+//		printf ("01");
+//		a=a+1;
+//	}
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main() {
+//	int cm=170;   //强制类型转换，本身那个小数放下面了，所以差不为0
+////	scanf("%d",&cm);
+//	int foot=cm/30.48;
+//	int inch=((cm/30.48)-foot)*12;
+//	printf("%d %d",foot,inch);
+//}
+
+
